62e375a5d12e10ed0a7964ebf299670a69c29fab
Turner Naef <Turner.Naef@gmail.com>
2022-04-06 00:53:49 -0600

    Renamed sideChar to NPC for map purposes as well as added a map driver

---
+++ NPC.h
@@ -0,0 +1,41 @@
+#ifndef NPC_h
+#define NPC_h
+
+#include <iostream>
+#include <vector>
+
+#include "Weapon.h"
+
+class NPC { // Use read movies/ read ratings to read sidechar from file
+    private:
+        string name;
+        string species;
+        string planet;
+        string planetRegion;
+        Weapon weapon;
+        int hp;
+        bool friendly;
+        string description;
+        int damage;
+        int creditReward;
+
+    public:
+        string getName();
+        string getSpecies();
+        string getPlanet();
+        string getPlanetRegion();
+        Weapon getWeapon();
+        int getHp();
+        bool getFriendly();
+        string getDescription();
+        int getDamage();
+        int getCreditReward();
+
+
+};
+
+
+
+
+
+#endif
