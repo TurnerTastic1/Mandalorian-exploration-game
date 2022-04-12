057a212c417dea8f361bef15679fb468db364625
Turner Naef <Turner.Naef@gmail.com>
2022-04-12 11:20:09 -0600

    Issues

--- Mando.h
+++ Mando.h
@@ -1,6 +1,6 @@
 // Mando.cpp
 // CSCI 1300 Spring 2022
-// Author: Caroline Scheidt, Turner naef
+// Author: Caroline Scheidt, Turner Naef
 // Recitation: 202 - TA Alexander Ray
 // Project 3 
 
--- NPC.h
+++ NPC.h
@@ -21,7 +21,7 @@ class NPC { // Use read movies/ read ratings to read sidechar from file
         string species;
         string planet;
         string planetRegion;
-        Weapon weapon;
+        //Weapon weapon;
         int hp;
         bool friendly;
         string description;
@@ -34,7 +34,7 @@ class NPC { // Use read movies/ read ratings to read sidechar from file
         string getSpecies();
         string getPlanet();
         string getPlanetRegion();
-        Weapon getWeapon();
+        //Weapon getWeapon();
         int getHP();
         bool getFriendly();
         string getDescription();
