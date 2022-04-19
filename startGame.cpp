e4baf2a4d34a9c79e4489e9b9fb92dc0f71a39f6
Turner Naef <Turner.Naef@gmail.com>
2022-04-18 18:41:56 -0600

    Added split, to lower and read ratings in startgame

--- startGame.cpp
+++ startGame.cpp
@@ -311,3 +311,115 @@ int main(){
 
     return 0;
 }
+
+
+
+
+
+
+
+// int Blockbustr::readRatings(string fileName) {
+//     int maxRatings = sizeMovie;
+//     if (numUsers == sizeUser) {
+//         return -2;
+//     }
+//     ifstream fin;
+//     fin.open(fileName);
+//     if (fin.is_open() == false) {
+//         return -1;
+//     }
+//     string line = "";
+
+//     while (numUsers < sizeUser) {
+//         if (fin.eof()) {
+//             break;
+//         }
+//         getline(fin, line);
+//         if (line.empty()) {
+//             continue;
+//         }
+//         // cout << "Ratings from file: " << line << "endl";
+//         string arr[1+maxRatings];
+//         int rvSplit = split(line, ',', arr, 1+maxRatings);
+//         if (rvSplit >= 1) {
+//             int rates[maxRatings];
+//             for (int i = 1; i < rvSplit; i++) {
+//                 rates[i-1] = stoi(arr[i]);
+//             }
+//             User a(arr[0], rates, maxRatings);
+//             users[numUsers] = a;
+//             numUsers++;
+//         }
+//     }
+//     return numUsers;
+// }
+
+
+// int split(string inputString, char separator, string arr[], int size) {
+//     if (inputString == "") {
+//         return 0;
+//     }
+//     int num = 0;
+//     for (int i = 0; i < size; i++) {
+//         if (arr[i] != "") {
+//             num++;
+//         }
+//     }
+//     if (num >= size) {
+//         return -1;
+//     }
+//     int count = 0;
+//     for (int i = 0; i < inputString.length(); i++) {
+//         if (inputString[i] == separator) {
+//             count++;
+//         }
+//     }
+//     if (count == 0) {
+//         arr[0] = inputString;
+//         return 1;
+//     } else if (count+1 > size) {
+//         int i = 0;
+//         inputString = inputString + string(1, separator);
+//         while (i < size) {
+//             string temp = "";
+//             for (int k = 0; k < inputString.length(); k++) {
+//                 if (inputString[k] == separator) {
+//                     temp = inputString.substr(0, k);
+//                     inputString = inputString.substr(k + 1, inputString.length()-(k+1));
+//                     break;
+//                 }
+//             }
+//             arr[i] = temp;
+//             i++;
+//         }
+//         return -1;
+//     } else {
+//         int i = 0;
+//         int count = 0;
+//         inputString = inputString + string(1, separator);
+//         while (i < size) {
+//             string temp = "";
+//             for (int k = 0; k < inputString.length(); k++) {
+//                 if (inputString[k] == separator) {
+//                     count++;
+//                     temp = inputString.substr(0, k);
+//                     inputString = inputString.substr(k + 1, inputString.length()-(k+1));
+//                     break;
+//                 }
+//             }
+//             arr[i] = temp;
+//             i++;
+//         }
+//         return count;
+//     }
+//     return 0;
+// }
+
+// string toLower(string s) {
+//     for (int i = 0; i < s.length(); i++) {
+//         if ((int(s[i]) >= 65) && (int(s[i]) <= 90)) {
+//             s[i] = char(int(s[i]) + 32);
+//         }
+//     }
+//     return s;
+// }
